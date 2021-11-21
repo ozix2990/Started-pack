9e6acc679d3a6d34cd7e5b073cf7f202db100f76
Alexey <ozix2990@gmail.com>
2021-11-21 15:57:57 +0300

    fix package.json and webp

--- src/modules/webp.js
+++ src/modules/webp.js
@@ -1,19 +1,20 @@
 function addWebp(callback) {
+	function testWebP(callback) {
+		var webP = new Image();
+		webP.onload = webP.onerror = function () {
+			callback(webP.height == 2);
+		};
+		webP.src =
+			"data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+	}
 
-    const webP = new Image();
-    webP.onload = webP.onerror = function () {
-    callback(webP.height == 2);
-    };
-    webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-    }
-    
-    addWebp(function (support) {
-    
-    if (support == true) {
-    document.querySelector('body').classList.add('webp');
-    }else{
-    document.querySelector('body').classList.add('no-webp');
-    }
-    });
+	testWebP(function (support) {
+		if (support == true) {
+			document.querySelector("body").classList.add("webp");
+		} else {
+			document.querySelector("body").classList.add("no-webp");
+		}
+	});
+}
 
 export default addWebp;
